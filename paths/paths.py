4f5c49cf0b0bbe0d42d5810b9370fe9457df495c
VishalNadig <nadigvishal@gmail.com>
2023-01-17 18:48:55 -0700

    Changed the paths file in paths folder

--- paths/paths.py
+++ paths/paths.py
@@ -1,4 +1,8 @@
 # LOGFILE = r"/Users/akshathanadig/Downloads/Education/Computer Science/Python/Trading Bot/LOGFILE_MAC.log"
-LOGFILE = r"/home/pi/python_projects/trading_bot/LOGFILE.log"
-CONFIG_FILE = r"/Users/akshathanadig/Downloads/git/megamind_config.yaml"
-ORDER_HISTORY_FILE = r"/home/pi/python_programs/trading_bot/order_history.csv"
+# LOGFILE = r"/home/pi/python_projects/trading_bot/LOGFILE.log"
+# CONFIG_FILE = r"/Users/akshathanadig/Downloads/git/megamind_config.yaml"
+# ORDER_HISTORY_FILE = r"/home/pi/python_programs/trading_bot/order_history.csv"
+
+LOGFILE = r"C:\Users\nadig\git\trading_bot\LOGFILE.log"
+CONFIG_FILE = r"C:\Users\nadig\git\megamind_config.yaml"
+ORDER_HISTORY_FILE = r"C:\Users\nadig\git\trading_bot\order_history.csv"
